7b30593cdf0084bc0b1560a157e883982eeb491a
Irchik512 <chornairchik@gmail.com>
2022-01-30 16:24:40 +0200

    add modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,22 @@ import { Component } from 'react';
 import Searchbar from 'components/Searchbar/Searchbar';
 import Modal from 'components/Modal/Modal';
 export class App extends Component {
-  state = {};
+  state = {
+    shoowModal: true,
+  };
   handleSubmit = data => {
     console.log(data);
   };
-
+  togleModal = () => {
+    this.setState(({ shoowModal }) => ({ shoowModal: !shoowModal }));
+  };
   render() {
+    const { shoowModal } = this.state;
     return (
-      <App>
+      <div className="app">
         <Searchbar onSubmit={this.handleSubmit} />
-        <Modal />
-      </App>
+        {shoowModal && <Modal onClose={this.togleModal} />}
+      </div>
     );
   }
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import 'style.css';
 
+const modalRoot = document.querySelector('#modal-root');
 class Modal extends Component {
   componentDidMount() {
     console.log('Modal componentDidApdate');
+    window.addEventListener('keydown', this.handleKeyDown);
   }
   componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
     console.log('Modal componentWillUnmount');
   }
+  handleKeyDown = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  handleOverlayClick = evt => {
+    if (evt.currentTarget === evt.target) {
+      this.props.onClose();
+    }
+  };
+
   render() {
-    return (
-      <div class="overlay">
-        <div class="modal">
+    return createPortal(
+      <div className="overlay" onClick={this.handleOverlayClick}>
+        <div className="modal">
+          {this.props.children}
           <img src="" alt="" />
         </div>
-      </div>
+      </div>,
+      modalRoot
     );
   }
 }
